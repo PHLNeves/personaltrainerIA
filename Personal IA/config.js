d6a56566eb0130d10b3f079beac091373745e2ef
Pedro Neves <pedro565_henrique@hotmail.com>
2026-02-21 15:00:49 -0300

    Update config.js

--- Personal IA/config.js
+++ Personal IA/config.js
@@ -1,4 +1,4 @@
 // Definimos a chave numa variável global que o HTML conseguirá ler
 window.GEMINI_CONFIG = {
-    apiKey: "AIzaSyAxEfg5Xg_TPnE2ybJWv9FSNPgABEqNGig"
+    apiKey: "Adicione a sua APIkey do google aqui "
 };
